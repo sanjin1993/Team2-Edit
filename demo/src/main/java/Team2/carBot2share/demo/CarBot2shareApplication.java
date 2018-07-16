3d683992745cb32c696329c4e6587dffb0259caa
sanjin <sanjin.zacinovic@edu.fit.ba>
2018-07-16 14:26:19 +0200

    init project

---
+++ demo/src/main/java/Team2/carBot2share/demo/CarBot2shareApplication.java
@@ -0,0 +1,12 @@
+package Team2.carBot2share.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBot2shareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBot2shareApplication.class, args);
+	}
+}
---
+++ demo/src/test/java/Team2/carBot2share/demo/CarBot2shareApplicationTests.java
@@ -0,0 +1,16 @@
+package Team2.carBot2share.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarBot2shareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
